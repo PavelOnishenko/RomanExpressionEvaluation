d2291786b6562eb867e1039b47befda73edadc11
Quest <Quest@MEGACLIFF>
2023-02-15 00:07:12 +0100

    Parsing V.

--- PlumsailTest/Tests.cs
+++ PlumsailTest/Tests.cs
@@ -7,18 +7,17 @@ public class Tests
     [TestCase("I")]
     [TestCase("II")]
     [TestCase("IV")]
-    public void LiteralParsing(string romanNumber)
-    {
+    [TestCase("V")]
+    public void LiteralParsing(string romanNumber) => 
         Assert.That(Evaluate(romanNumber), Is.EqualTo(romanNumber));
-    }
 
     private string Evaluate(string input)
     {
         var number = Parse.String("IV").Return(4).Or(Parse.String("I").Return(1))
-            .Many().Select(x => x.Sum()).Parse(input);
+            .Or(Parse.String("V").Return(5)).Many().Select(x => x.Sum()).Parse(input);
         return number switch 
         { 
-            1 => "I", 2 => "II", 4 => "IV", 
+            1 => "I", 2 => "II", 4 => "IV", 5 => "V",
             _ => throw new Exception($"Can't convert {number} to roman number.") 
         };
     }
