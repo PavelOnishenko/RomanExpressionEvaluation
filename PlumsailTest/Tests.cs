b5f5549096c409825f246ac698b1ed0b1368ab3d
Quest <Quest@MEGACLIFF>
2023-02-15 00:23:30 +0100

    Parsing X.

--- PlumsailTest/Tests.cs
+++ PlumsailTest/Tests.cs
@@ -1,4 +1,5 @@
 using Sprache;
+using System.Data;
 
 namespace PlumsailTest;
 
@@ -9,17 +10,21 @@ public class Tests
     [TestCase("IV")]
     [TestCase("V")]
     [TestCase("IX")]
-    public void LiteralParsing(string romanNumber) => 
+    [TestCase("X")]
+    public void OneDigit(string romanNumber) => 
         Assert.That(Evaluate(romanNumber), Is.EqualTo(romanNumber));
 
     private string Evaluate(string input)
     {
-        var number = Parse.String("IV").Return(4).Or(Parse.String("IX").Return(9))
-            .Or(Parse.String("I").Return(1)).Or(Parse.String("V").Return(5))
-            .Many().Select(x => x.Sum()).Parse(input);
+        var digitInfo = new (string roman, int val)[]
+        {("IX", 9), ("I", 1), ("V", 5), ("X", 10)};
+        var parser = Parse.String("IV").Return(4);
+        foreach(var tuple in digitInfo)
+            parser = parser.Or(Parse.String(tuple.roman).Return(tuple.val));
+        var number = parser.Many().Select(x => x.Sum()).Parse(input);
         return number switch 
         { 
-            1 => "I", 2 => "II", 4 => "IV", 5 => "V", 9 => "IX",
+            1 => "I", 2 => "II", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X",
             _ => throw new Exception($"Can't convert {number} to roman number.") 
         };
     }
