fb9a109117a425c6d0d1e5db48d43d20ab3fc629
Quest <Quest@MEGACLIFF>
2023-02-15 00:38:19 +0100

    Parsing XL.

--- PlumsailTest/Tests.cs
+++ PlumsailTest/Tests.cs
@@ -11,21 +11,36 @@ public class Tests
     [TestCase("V")]
     [TestCase("IX")]
     [TestCase("X")]
-    public void OneDigit(string romanNumber) => 
+    [TestCase("XL")]
+    public void OneDigit(string romanNumber) =>
         Assert.That(Evaluate(romanNumber), Is.EqualTo(romanNumber));
 
     private string Evaluate(string input)
     {
         var digitInfo = new (string roman, int val)[]
-        {("IX", 9), ("I", 1), ("V", 5), ("X", 10)};
+        {("IX", 9), ("XL", 40), ("I", 1), ("V", 5), ("X", 10)};
         var parser = Parse.String("IV").Return(4);
-        foreach(var tuple in digitInfo)
+        foreach (var tuple in digitInfo)
             parser = parser.Or(Parse.String(tuple.roman).Return(tuple.val));
         var number = parser.Many().Select(x => x.Sum()).Parse(input);
-        return number switch 
-        { 
-            1 => "I", 2 => "II", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X",
-            _ => throw new Exception($"Can't convert {number} to roman number.") 
-        };
+        return IntToRoman(number);
+    }
+
+    private string IntToRoman(int number)
+    {
+        var result = "";
+        var digitInfo = new (string roman, int val)[]
+        { ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1) };
+        var i = 0;
+        while (number > 0)
+        {
+            if (number >= digitInfo[i].val)
+            {
+                number -= digitInfo[i].val;
+                result += digitInfo[i].roman;
+            }
+            else i++;
+        }
+        return result;
     }
 }
