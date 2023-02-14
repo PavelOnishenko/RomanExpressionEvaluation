4008d01aaf4132be9cd0cca2552ca917977c0438
Quest <Quest@MEGACLIFF>
2023-02-15 00:04:25 +0100

    Parsing IV.

--- PlumsailTest/Tests.cs
+++ PlumsailTest/Tests.cs
@@ -4,16 +4,22 @@ namespace PlumsailTest;
 
 public class Tests
 {
-    [Test]
-    public void ParseIi() => Assert.That(Evaluate("II"), Is.EqualTo("II"));
-
-    [Test]
-    public void ParseI() => Assert.That(Evaluate("I"), Is.EqualTo("I"));
+    [TestCase("I")]
+    [TestCase("II")]
+    [TestCase("IV")]
+    public void LiteralParsing(string romanNumber)
+    {
+        Assert.That(Evaluate(romanNumber), Is.EqualTo(romanNumber));
+    }
 
     private string Evaluate(string input)
     {
-        var number = Parse.String("I").Return(1).Many().Select(x => x.Sum()).Parse(input);
+        var number = Parse.String("IV").Return(4).Or(Parse.String("I").Return(1))
+            .Many().Select(x => x.Sum()).Parse(input);
         return number switch 
-        { 1 => "I", 2 => "II", _ => throw new Exception($"Can't convert {number} to roman number.") };
+        { 
+            1 => "I", 2 => "II", 4 => "IV", 
+            _ => throw new Exception($"Can't convert {number} to roman number.") 
+        };
     }
 }
